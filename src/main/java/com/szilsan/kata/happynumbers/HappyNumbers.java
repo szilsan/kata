8cb5051b8fced4f7213191305e34b3ae22f9f263
szilsan <szilsan@gmail.com>
2018-05-10 22:19:04 +0200

    Init commit

---
+++ src/main/java/com/szilsan/kata/happynumbers/HappyNumbers.java
@@ -0,0 +1,14 @@
+package com.szilsan.kata.happynumbers;
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+public class HappyNumbers {
+
+    public static boolean isHappyNumber(long number) {
+        throw new NotImplementedException();
+    }
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
---
+++ src/test/java/com/szilsan/kata/happynumbers/HappyNumbersTest.java
@@ -0,0 +1,11 @@
+package com.szilsan.kata.happynumbers;
+
+import org.junit.jupiter.api.Assertions;
+
+class HappyNumbersTest {
+
+    @org.junit.jupiter.api.Test
+    void isHappyNumber() {
+        Assertions.assertTrue(true);
+    }
+}
