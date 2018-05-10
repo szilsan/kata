bdd21b7677f5b52475dd38995dd8596533110f19
szilsan <szilsan@gmail.com>
2018-05-10 22:52:23 +0200

    Basic implementations.

--- src/main/java/com/szilsan/kata/happynumbers/HappyNumbers.java
+++ src/main/java/com/szilsan/kata/happynumbers/HappyNumbers.java
@@ -1,12 +1,58 @@
 package com.szilsan.kata.happynumbers;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
 public class HappyNumbers {
 
-    public static boolean isHappyNumber(long number) {
-        throw new NotImplementedException();
+    // based on characters
+    public static boolean isHappyNumberBasedOnChar(long number) {
+        if (number == 1) {
+            return true;
+        }
+
+        long sum = 0;
+        for (char c: ("" + number).toCharArray()) {
+            sum +=  Math.pow(Long.valueOf("" + c), 2);
+        }
+
+        return isHappyNumberBasedOnChar(sum);
     }
+
+    public static boolean isHappyNumber(long number) {
+        if (number == 1) {
+            return true;
+        }
+
+        long sum = 0;
+        long processedNumber = number;
+        while (processedNumber > 0) {
+            long i = processedNumber % 10;
+            processedNumber /= 10;
+            sum += Math.pow(i, 2);
+        }
+
+        return isHappyNumber(sum);
+    }
+
+    public static boolean isHappyNumberNonRecursive(long number) {
+        if (number == 1) {
+            return true;
+        }
+        long processedNumber = number;
+        long sum = 0;
+
+        do {
+            sum = 0;
+            while (processedNumber > 0) {
+                long i = processedNumber % 10;
+                processedNumber /= 10;
+                sum += Math.pow(i, 2);
+            }
+            processedNumber = sum;
+        } while (sum != 1);
+
+        return true;
+    }
+
+
 
     public static void main(String[] args) {
         System.out.println("hello world");
--- src/test/java/com/szilsan/kata/happynumbers/HappyNumbersTest.java
+++ src/test/java/com/szilsan/kata/happynumbers/HappyNumbersTest.java
@@ -1,11 +1,55 @@
 package com.szilsan.kata.happynumbers;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 class HappyNumbersTest {
 
-    @org.junit.jupiter.api.Test
+    @Test
+    void isHappyNumberBasedOnChar() {
+        Assertions.assertTrue(HappyNumbers.isHappyNumberBasedOnChar(1));
+        Assertions.assertTrue(HappyNumbers.isHappyNumberBasedOnChar(7));
+        Assertions.assertTrue(HappyNumbers.isHappyNumberBasedOnChar(10));
+        Assertions.assertTrue(HappyNumbers.isHappyNumberBasedOnChar(13));
+        Assertions.assertTrue(HappyNumbers.isHappyNumberBasedOnChar(19));
+        Assertions.assertTrue(HappyNumbers.isHappyNumberBasedOnChar(23));
+        Assertions.assertTrue(HappyNumbers.isHappyNumberBasedOnChar(28));
+        Assertions.assertTrue(HappyNumbers.isHappyNumberBasedOnChar(31));
+        Assertions.assertTrue(HappyNumbers.isHappyNumberBasedOnChar(32));
+        Assertions.assertTrue(HappyNumbers.isHappyNumberBasedOnChar(97));
+        Assertions.assertTrue(HappyNumbers.isHappyNumberBasedOnChar(100));
+        Assertions.assertTrue(HappyNumbers.isHappyNumberBasedOnChar(86));
+    }
+
+    @Test
     void isHappyNumber() {
-        Assertions.assertTrue(true);
+        Assertions.assertTrue(HappyNumbers.isHappyNumber(1));
+        Assertions.assertTrue(HappyNumbers.isHappyNumber(7));
+        Assertions.assertTrue(HappyNumbers.isHappyNumber(10));
+        Assertions.assertTrue(HappyNumbers.isHappyNumber(13));
+        Assertions.assertTrue(HappyNumbers.isHappyNumber(19));
+        Assertions.assertTrue(HappyNumbers.isHappyNumber(23));
+        Assertions.assertTrue(HappyNumbers.isHappyNumber(28));
+        Assertions.assertTrue(HappyNumbers.isHappyNumber(31));
+        Assertions.assertTrue(HappyNumbers.isHappyNumber(32));
+        Assertions.assertTrue(HappyNumbers.isHappyNumber(97));
+        Assertions.assertTrue(HappyNumbers.isHappyNumber(100));
+        Assertions.assertTrue(HappyNumbers.isHappyNumber(86));
+    }
+
+    @Test
+    void isHappyNumberNonRecursive() {
+        Assertions.assertTrue(HappyNumbers.isHappyNumberNonRecursive(1));
+        Assertions.assertTrue(HappyNumbers.isHappyNumberNonRecursive(7));
+        Assertions.assertTrue(HappyNumbers.isHappyNumberNonRecursive(10));
+        Assertions.assertTrue(HappyNumbers.isHappyNumberNonRecursive(13));
+        Assertions.assertTrue(HappyNumbers.isHappyNumberNonRecursive(19));
+        Assertions.assertTrue(HappyNumbers.isHappyNumberNonRecursive(23));
+        Assertions.assertTrue(HappyNumbers.isHappyNumberNonRecursive(28));
+        Assertions.assertTrue(HappyNumbers.isHappyNumberNonRecursive(31));
+        Assertions.assertTrue(HappyNumbers.isHappyNumberNonRecursive(32));
+        Assertions.assertTrue(HappyNumbers.isHappyNumberNonRecursive(97));
+        Assertions.assertTrue(HappyNumbers.isHappyNumberNonRecursive(100));
+        Assertions.assertTrue(HappyNumbers.isHappyNumberNonRecursive(86));
     }
 }
