f432602b137154cd628f1b92c98c50b3936c973a
szilsan <szilsan@gmail.com>
2018-05-10 23:24:21 +0200

    Test refactor

--- src/test/java/com/szilsan/kata/happynumbers/HappyNumbersTest.java
+++ src/test/java/com/szilsan/kata/happynumbers/HappyNumbersTest.java
@@ -6,50 +6,16 @@ import org.junit.jupiter.api.Test;
 class HappyNumbersTest {
 
     @Test
-    void isHappyNumberBasedOnChar() {
-        Assertions.assertTrue(HappyNumbers.isHappyNumberBasedOnChar(1));
-        Assertions.assertTrue(HappyNumbers.isHappyNumberBasedOnChar(7));
-        Assertions.assertTrue(HappyNumbers.isHappyNumberBasedOnChar(10));
-        Assertions.assertTrue(HappyNumbers.isHappyNumberBasedOnChar(13));
-        Assertions.assertTrue(HappyNumbers.isHappyNumberBasedOnChar(19));
-        Assertions.assertTrue(HappyNumbers.isHappyNumberBasedOnChar(23));
-        Assertions.assertTrue(HappyNumbers.isHappyNumberBasedOnChar(28));
-        Assertions.assertTrue(HappyNumbers.isHappyNumberBasedOnChar(31));
-        Assertions.assertTrue(HappyNumbers.isHappyNumberBasedOnChar(32));
-        Assertions.assertTrue(HappyNumbers.isHappyNumberBasedOnChar(97));
-        Assertions.assertTrue(HappyNumbers.isHappyNumberBasedOnChar(100));
-        Assertions.assertTrue(HappyNumbers.isHappyNumberBasedOnChar(86));
+    void isHappyNumberTest() {
+        long goodTestNumbers[] = new long[] {1, 7, 10, 13, 19, 23, 28, 31, 32, 44, 49, 68, 70, 79, 82, 86, 91, 94, 97, 100};
+        for (long number: goodTestNumbers) {
+            doAllTestForANumber(number);
+        }
     }
 
-    @Test
-    void isHappyNumber() {
-        Assertions.assertTrue(HappyNumbers.isHappyNumber(1));
-        Assertions.assertTrue(HappyNumbers.isHappyNumber(7));
-        Assertions.assertTrue(HappyNumbers.isHappyNumber(10));
-        Assertions.assertTrue(HappyNumbers.isHappyNumber(13));
-        Assertions.assertTrue(HappyNumbers.isHappyNumber(19));
-        Assertions.assertTrue(HappyNumbers.isHappyNumber(23));
-        Assertions.assertTrue(HappyNumbers.isHappyNumber(28));
-        Assertions.assertTrue(HappyNumbers.isHappyNumber(31));
-        Assertions.assertTrue(HappyNumbers.isHappyNumber(32));
-        Assertions.assertTrue(HappyNumbers.isHappyNumber(97));
-        Assertions.assertTrue(HappyNumbers.isHappyNumber(100));
-        Assertions.assertTrue(HappyNumbers.isHappyNumber(86));
-    }
-
-    @Test
-    void isHappyNumberNonRecursive() {
-        Assertions.assertTrue(HappyNumbers.isHappyNumberNonRecursive(1));
-        Assertions.assertTrue(HappyNumbers.isHappyNumberNonRecursive(7));
-        Assertions.assertTrue(HappyNumbers.isHappyNumberNonRecursive(10));
-        Assertions.assertTrue(HappyNumbers.isHappyNumberNonRecursive(13));
-        Assertions.assertTrue(HappyNumbers.isHappyNumberNonRecursive(19));
-        Assertions.assertTrue(HappyNumbers.isHappyNumberNonRecursive(23));
-        Assertions.assertTrue(HappyNumbers.isHappyNumberNonRecursive(28));
-        Assertions.assertTrue(HappyNumbers.isHappyNumberNonRecursive(31));
-        Assertions.assertTrue(HappyNumbers.isHappyNumberNonRecursive(32));
-        Assertions.assertTrue(HappyNumbers.isHappyNumberNonRecursive(97));
-        Assertions.assertTrue(HappyNumbers.isHappyNumberNonRecursive(100));
-        Assertions.assertTrue(HappyNumbers.isHappyNumberNonRecursive(86));
+    private void doAllTestForANumber(long number) {
+        Assertions.assertTrue(HappyNumbers.isHappyNumberBasedOnChar(number));
+        Assertions.assertTrue(HappyNumbers.isHappyNumber(number));
+        Assertions.assertTrue(HappyNumbers.isHappyNumberNonRecursive(number));
     }
 }
