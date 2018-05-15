7512e0226aea7b0b0caec8978380245825415bcf
szilsan <szilsan@gmail.com>
2018-05-15 23:09:55 +0200

    Calculator kata: final version based on regex

--- src/main/java/com/szilsan/kata/calculator/Calculator.java
+++ src/main/java/com/szilsan/kata/calculator/Calculator.java
@@ -1,57 +1,48 @@
 package com.szilsan.kata.calculator;
 
+import java.util.function.BiFunction;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Calculator {
 
-    private static final Pattern patternDevide = Pattern.compile("(\\d+(?:\\.\\d+)?)([/])([-]*\\d+(?:\\.\\d+)?)");
-    private static final Pattern patternMultiply = Pattern.compile("(\\d+(?:\\.\\d+)?)([*])([-]*\\d+(?:\\.\\d+)?)");
-    private static final Pattern patternAdd = Pattern.compile("(\\d+(?:\\.\\d+)?)([+])([-]*\\d+(?:\\.\\d+)?)");
-    private static final Pattern patternSub = Pattern.compile("([-]*\\d+(?:\\.\\d+)?)([-])([-]*\\d+(?:\\.\\d+)?)");
+    private static final String NUMBER_EXPRESSION = "([-|+)]*\\d+(?:\\.\\d+)?(?:[E][-]?\\d+)?)";
 
+    private static final Pattern patternDivide = Pattern.compile(NUMBER_EXPRESSION + "([/])" + NUMBER_EXPRESSION);
+    private static final Pattern patternMultiply = Pattern.compile(NUMBER_EXPRESSION + "([*])" + NUMBER_EXPRESSION);
+    private static final Pattern patternAdd = Pattern.compile(NUMBER_EXPRESSION + "([+])" + NUMBER_EXPRESSION);
+    private static final Pattern patternSub = Pattern.compile(NUMBER_EXPRESSION + "([-])" + NUMBER_EXPRESSION);
 
     public static Double evaluate(final String expression) {
-        String processedExpression = expression;
-        processedExpression = processedExpression.replaceAll(" ", "");
+        String processedExpression = collapseSigns(expression);
 
-        // devide
-        Matcher m = patternDevide.matcher(processedExpression);
-        while (m.find()) {
-            String expr = m.group(0);
-            String value = "" + (Double.valueOf(m.group(1)) / Double.valueOf(m.group(3)));
-            processedExpression = processedExpression.replace(expr, value);
-            m = patternDevide.matcher(processedExpression);
-        }
-
-        // multiply
-        m = patternMultiply.matcher(processedExpression);
-        while (m.find()) {
-            String expr = m.group(0);
-            String value = "" + (Double.valueOf(m.group(1)) * Double.valueOf(m.group(3)));
-            processedExpression = processedExpression.replace(expr, value);
-            m = patternMultiply.matcher(processedExpression);
-        }
-
-        // sum
-        m = patternAdd.matcher(processedExpression);
-        while (m.find()) {
-            String expr = m.group(0);
-            String value = "" + (Double.valueOf(m.group(1)) + Double.valueOf(m.group(3)));
-            processedExpression = processedExpression.replace(expr, value);
-            m = patternAdd.matcher(processedExpression);
-        }
-
-        // sub
-        m = patternSub.matcher(processedExpression);
-        while (m.find()) {
-            String expr = m.group(0);
-            String value = "" + (Double.valueOf(m.group(1)) - Double.valueOf(m.group(3)));
-            processedExpression = processedExpression.replace(expr, value);
-            m = patternSub.matcher(processedExpression);
-        }
+        processedExpression = calculateOperandOverExpression(patternDivide, processedExpression, (a, b) -> (a/b));
+        processedExpression = calculateOperandOverExpression(patternMultiply, processedExpression, (a, b) -> (a*b));
+        processedExpression = calculateOperandOverExpression(patternAdd, processedExpression, (a, b) -> (a+b));
+        processedExpression = calculateOperandOverExpression(patternSub, processedExpression, (a, b) -> (a-b));
 
         return Double.valueOf(processedExpression);
+    }
 
+    private static String calculateOperandOverExpression(final Pattern pattern, final String processedExpression, final BiFunction<Double, Double, Double> func) {
+        String tmpExpression = processedExpression;
+        Matcher m = pattern.matcher(tmpExpression);
+        while (m.find()) {
+            String expr = m.group(0);
+            Double value = func.apply(Double.valueOf(m.group(1)), Double.valueOf(m.group(3)));
+            tmpExpression = tmpExpression.replace(expr, (value > 0 ? "+" + value : "" + value));
+            m = pattern.matcher(tmpExpression);
+        }
+        return tmpExpression;
+    }
+
+    private static String collapseSigns(String expression) {
+        String tmp = expression;
+        tmp = tmp.replace(" ", "");
+        tmp = tmp.replace("-+", "-");
+        tmp = tmp.replace("+-", "-");
+        tmp = tmp.replace("--", "+");
+        tmp = tmp.replace("++", "+");
+        return tmp;
     }
 }
--- src/test/java/com/szilsan/kata/calculator/CalculatorTest.java
+++ src/test/java/com/szilsan/kata/calculator/CalculatorTest.java
@@ -6,12 +6,12 @@ import static org.junit.jupiter.api.Assertions.*;
 
 
 public class CalculatorTest {
-    //@Test
+    @Test
     public void simpleLiteral() {
         assertEquals(new Double(127), Calculator.evaluate("127"));
     }
 
-    //@Test
+    @Test
     public void subtractionAndAddition() {
         assertEquals(new Double(5), Calculator.evaluate("2 + 3"));
         assertEquals(new Double(20), Calculator.evaluate("2 + 3 + 7 +8"));
@@ -20,19 +20,21 @@ public class CalculatorTest {
 
     @Test
     public void divisionAndMultiplication() {
-        //assertEquals(new Double(10), Calculator.evaluate("10*5/5"));
-        //assertEquals(new Double(-10), Calculator.evaluate("-10*5/5"));
-        //assertEquals(new Double(-10), Calculator.evaluate("10*5/-5"));
-        //assertEquals(new Double(-10), Calculator.evaluate("2.4200000000000004*3.3"));
-        assertEquals(new Double(6), Calculator.evaluate("1.1 * 2.2 * 3.3"));
+        assertEquals(new Double(10), Calculator.evaluate("10*5/5"));
+        assertEquals(new Double(-10), Calculator.evaluate("-10*5/5"));
+        assertEquals(new Double(-10), Calculator.evaluate("10*5/-5"));
+        assertEquals(new Double(1.710540003E9), Calculator.evaluate("2118952920 - 1976377588 - -1567964671"));
+        assertEquals(new Double(8), Calculator.evaluate("2 + 3 * 4 / 3 - 6 / 3 * 3 + 8"));
+        assertEquals(new Double(0), Calculator.evaluate("2 - 3 * 4 / 3 - 6 / 3 * 3 + 8"));
+
     }
 
-    //@Test
+    @Test
     public void allMixed() {
         assertEquals(new Double(13), Calculator.evaluate("2 / 2 + 3 * 4"));
     }
 
-    //@Test
+    @Test
     public void floats() {
         assertEquals(new Double(0), Calculator.evaluate("7.7 - 3.3 - 4.4"));
     }
