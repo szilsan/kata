10a6c223a6db4682637cdf46abed075a4c1f4781
szilsan <szilsan@gmail.com>
2018-05-22 20:05:17 +0200

    Kata: Battlefield validator - init state

---
+++ src/main/java/com/szilsan/kata/battleshipfieldvalidator/BattleField.java
@@ -0,0 +1,9 @@
+package com.szilsan.kata.battleshipfieldvalidator;
+
+public class BattleField {
+
+    public static boolean fieldValidator(int[][] field) {
+        // your code here!
+        return false;
+    }
+}
---
+++ src/test/java/com/szilsan/kata/battleshipfieldvalidator/BattleFieldTest.java
@@ -0,0 +1,24 @@
+package com.szilsan.kata.battleshipfieldvalidator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class BattleFieldTest {
+
+    private static int[][] battleField = {{1, 0, 0, 0, 0, 1, 1, 0, 0, 0},
+            {1, 0, 1, 0, 0, 0, 0, 0, 1, 0},
+            {1, 0, 1, 0, 1, 1, 1, 0, 1, 0},
+            {1, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+            {0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+            {0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
+
+    @Test
+    public void SampleTest() {
+        assertEquals(true, BattleField.fieldValidator(battleField));
+    }
+}
