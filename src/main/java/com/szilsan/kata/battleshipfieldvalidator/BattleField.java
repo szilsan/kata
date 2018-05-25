ee71e9a04fca0ec61d082405c53255f7cfff2a0a
szilsan <szilsan@gmail.com>
2018-05-25 19:48:38 +0200

    Kata: Battlefield validator - done

--- src/main/java/com/szilsan/kata/battleshipfieldvalidator/BattleField.java
+++ src/main/java/com/szilsan/kata/battleshipfieldvalidator/BattleField.java
@@ -1,32 +1,60 @@
 package com.szilsan.kata.battleshipfieldvalidator;
 
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 public class BattleField {
 
-    final static Set<Shape> shapes = new HashSet<>();
+    static Set<Shape> shapes = new HashSet<>();
 
     public static boolean fieldValidator(int[][] field) {
 
         int size = field[0].length;
+        shapes = new HashSet<>();
 
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                if (field[i][j] == 1) {
-                    Point p = new Point(i, j);
-                    processField(p);
+        try {
+            for (int i = 0; i < size; i++) {
+                for (int j = 0; j < size; j++) {
+                    if (field[i][j] == 1) {
+                        Point p = new Point(i, j);
+                        processField(p);
+                    }
                 }
+            }
+        } catch (Exception ex) {
+            return false;
+        }
+
+        // checking sizes
+        int count4 = 0;
+        int count3 = 0;
+        int count2 = 0;
+        int count1 = 0;
+        for (Shape s: shapes) {
+            if (s.getPoints().size() == 4) {
+                count4 ++;
+            } else if (s.getPoints().size() == 3) {
+                count3 ++;
+            } if (s.getPoints().size() == 2) {
+                count2 ++;
+            } if (s.getPoints().size() == 1) {
+                count1 ++;
             }
         }
 
-        return false;
+        return (count4 == 1 && count3 == 2 && count2 ==3 && count1 == 4);
     }
 
     private static void processField(final Point p) {
         int hasAffectedShape = 0;
         for (Shape shape : shapes) {
-            hasAffectedShape += (shape.validPointForExtension(p) ? 1 : 0);
+            int ext = (shape.validPointForExtension(p));
+            if (ext == -1) {
+                throw new RuntimeException("Invalid point.");
+            }
+            hasAffectedShape += ext;
         }
 
         if (hasAffectedShape == 0) {
@@ -42,6 +70,9 @@ public class BattleField {
 }
 
 class Shape {
+    private static final Set<PointTouch>  VALID_NEOGHTBOURS = new HashSet<>(Arrays.asList(PointTouch.T, PointTouch.B, PointTouch.L, PointTouch.R));
+    private static final Set<PointTouch>  INVALID_NEOGHTBOURS = new HashSet<>(Arrays.asList(PointTouch.TR, PointTouch.TL, PointTouch.BL, PointTouch.BR));
+
     private Set<Point> points = new HashSet<>();
 
     public Shape() {
@@ -51,9 +82,59 @@ class Shape {
         return points;
     }
 
-    public boolean validPointForExtension(final Point p) {
-        // TODO
-        return false;
+    /**
+     * Valid a point as an extension
+     *
+     * @param p
+     * @return 0 :
+     */
+    public int validPointForExtension(final Point p) {
+
+        int isNeightbour = 0;
+        if (points.isEmpty()) {
+            isNeightbour = 0;
+        } else {
+
+            for (Point ip : points) {
+                int cp =isGoodNeightbour(ip, p);
+
+                if (cp == 1) {
+                    isNeightbour = 1;
+                } else if (cp == 0){
+                    //isNeightbour = 0;
+                } else {
+                    isNeightbour = -1;
+                    break;
+                }
+            }
+        }
+        if (isNeightbour == 1) {
+            this.getPoints().add(p);
+        }
+
+        // point - point
+        // not touched, invalid, valid
+        // valid for shape: true for each point: not touched and/or valid for each
+        return isNeightbour;
+    }
+
+    @Override
+    public String toString() {
+        return "Shape{" +
+                "points=" + points +
+                '}';
+    }
+
+    static int isGoodNeightbour(final Point p1, final Point p2) {
+        PointTouch orientation = p1.orientation(p2);
+        if (orientation != PointTouch.SAME && VALID_NEOGHTBOURS.contains(orientation)) {
+            return 1;
+        }
+
+        if (orientation != PointTouch.SAME && INVALID_NEOGHTBOURS.contains(orientation)) {
+            return -1;
+        }
+        return 0;
     }
 }
 
@@ -73,4 +154,76 @@ class Point {
     public int getY() {
         return y;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Point point = (Point) o;
+        return x == point.x &&
+                y == point.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+
+    @Override
+    public String toString() {
+        return "Point{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+
+    public PointTouch orientation(final Point p) {
+        if (this.equals(p)) {
+            return PointTouch.SAME;
+        }
+
+        if (this.getX() == p.getX() + 1) {
+            if (this.getY() == p.getY() + 1) {
+                return PointTouch.TL;
+            } else if (this.getY() == p.getY() - 1) {
+                return PointTouch.TR;
+            } else if (this.getY() == p.getY() - 1) {
+                return PointTouch.T;
+            } else {
+                return PointTouch.NOT;
+            }
+        }
+
+        if (this.getX() == p.getX() - 1) {
+            if (this.getY() == p.getY() + 1) {
+                return PointTouch.BL;
+            } else if (this.getY() == p.getY() - 1) {
+                return PointTouch.BR;
+            } else if (this.getY() == p.getY()) {
+                return PointTouch.B;
+            } else {
+                return PointTouch.NOT;
+            }
+        }
+
+        if (this.getY() == p.getY() + 1 && this.getX() == p.getX()) {
+            return PointTouch.L;
+        } else if (this.getY() == p.getY() - 1 && this.getX() == p.getX()) {
+            return PointTouch.R;
+        }
+        return PointTouch.NOT;
+    }
+}
+
+enum PointTouch {
+    T,
+    B,
+    L,
+    R,
+    TL,
+    TR,
+    BL,
+    BR,
+    NOT,
+    SAME
 }
--- src/test/java/com/szilsan/kata/battleshipfieldvalidator/BattleFieldTest.java
+++ src/test/java/com/szilsan/kata/battleshipfieldvalidator/BattleFieldTest.java
@@ -6,7 +6,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class BattleFieldTest {
 
-    private static int[][] battleField = {{1, 0, 0, 0, 0, 1, 1, 0, 0, 0},
+    private static int[][] battleFieldGood = {
+            {1, 0, 0, 0, 0, 1, 1, 0, 0, 0},
             {1, 0, 1, 0, 0, 0, 0, 0, 1, 0},
             {1, 0, 1, 0, 1, 1, 1, 0, 1, 0},
             {1, 0, 0, 0, 0, 0, 0, 0, 0, 0},
@@ -16,9 +17,48 @@ public class BattleFieldTest {
             {0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
             {0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
             {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
+    /*
+    1: 4
+    2: 3
+    3: 2
+    4: 1
+
+     */
+
+
+    private static int[][] battleFieldBad = {
+            {1, 0, 0, 0, 0, 1, 1, 0, 0, 0},
+            {1, 0, 1, 0, 0, 0, 0, 0, 1, 0},
+            {1, 0, 1, 0, 1, 1, 1, 0, 1, 0},
+            {1, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+            {0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+            {0, 0, 0, 0, 0, 1, 0, 0, 1, 0},
+            {0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
 
     @Test
-    public void SampleTest() {
-        assertEquals(true, BattleField.fieldValidator(battleField));
+    public void testGood() {
+        assertEquals(true, BattleField.fieldValidator(battleFieldGood));
     }
+
+    @Test
+    public void testBad() {
+        assertEquals(false, BattleField.fieldValidator(battleFieldBad));
+    }
+
+
+    //@Test
+    public void testShape() {
+        Shape sh = new Shape();
+
+        Point p1 = new Point(1,1);
+        Point p2 = new Point(2,1);
+        Point p3 = new Point(2,2);
+
+        assertTrue(sh.validPointForExtension(p1) == 0);
+        assertTrue(sh.validPointForExtension(p2) == 1);
+        assertTrue(sh.validPointForExtension(p3) == -1);
+}
 }
